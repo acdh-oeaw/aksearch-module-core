2ebccb974e6ddbb6c59d4d8955a3cabe82721ce9
Michael Birkner <michael.birkner@akwien.at>
2019-04-08 14:47:06 +0200

    Use custom Solr date field for getting new items

--- src/AkSearch/Controller/Plugin/NewItems.php
+++ src/AkSearch/Controller/Plugin/NewItems.php
@@ -1,12 +1,25 @@
 <?php
 namespace AkSearch\Controller\Plugin;
 
-use Zend\Config\Config;
-use Zend\Mvc\Controller\Plugin\AbstractPlugin;
-use Zend\Mvc\Plugin\FlashMessenger\FlashMessenger;
-
-
 class NewItems extends \VuFind\Controller\Plugin\NewItems
 {
+
+    /**
+     * Get a Solr filter to limit to the specified number of days.
+     * AK: Added possibility to use a custom Solr date field for the filter.
+     *
+     * @param int $range Days to search
+     *
+     * @return string
+     */
+    public function getSolrFilter($range)
+    {
+        $solrFieldConf = $this->config['solrfield'];
+        $solrField = (isset($solrFieldConf) && !empty($solrFieldConf))
+                     ? $solrFieldConf
+                     : 'first_indexed';
+
+        return $solrField.':[NOW-' . $range . 'DAY TO NOW]';
+    }
 
 }
