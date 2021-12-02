74b24f92dadf5515504d124233333bb7d84e7868
Michael Birkner <michael.birkner@akwien.at>
2021-12-02 16:09:04 +0100

    Removed "display all PHP errors" in module.config.php

--- config/module.config.php
+++ config/module.config.php
@@ -3,9 +3,9 @@
 namespace AkSearch\Module\Configuration;
 
 // Show PHP errors:
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(-1);
+//ini_set('display_errors', 1);
+//ini_set('display_startup_errors', 1);
+//error_reporting(-1);
 
 $config = [
     'controllers' => [
